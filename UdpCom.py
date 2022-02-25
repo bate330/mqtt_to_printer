104fcf9c6de06e4fe344088f2d96f1931fd2ba00
bate330 <bateu.email@yahoo.fr>
2022-02-25 08:41:20 +0000

    1.0.0 udp+serial

--- UdpCom.py
+++ UdpCom.py
@@ -1,8 +1,44 @@
 import socket
 
 class UdpCom:
-    @staticmethod
-    def tcp_sender(ip, port, msg):
+
+    def __init__(self, ip, port, msg):
+        self.ip = ip
+        self.port = port
+        self.msg = msg
+
+    def udp_sender(self):
+        stx = 2
+        devno = 0
+        frno = 1
+        cb = 57
+        eof = 13
+        dlen = self.__calc_dlen(self.msg)
+        chx = self.__calc_chx(devno, frno, cb, dlen, self.msg, eof)
+        etx = 3
+        frame = self.__get_frame(stx, devno, frno, dlen, cb, self.msg, eof, chx, etx)
+        print(frame)
         client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        client_socket.sendto(msg, (ip, port))
-        print(msg)
+        client_socket.sendto(frame, (self.ip, self.port))
+
+    def __get_frame(self, stx, devno, frno, dlen, cb, msg, eof, chx, etx):
+        return bytearray(bytes([stx]) + bytes([devno]) + bytes([frno]) + bytes([dlen]) + b'\x39' + msg + b'\x0d' + bytes([chx]) + bytes([etx]))
+
+    def __calc_dlen(self, msg):
+        return len(msg) + 2
+
+    def __calc_chx(self, devno, frno, cb, dlen, msg, eof):
+        sum = 0
+        for byte in msg:
+            sum += byte
+        sum = sum + devno + frno + cb + dlen + eof
+        result = 0 - sum
+        print(result & 0xFF)
+        return result & 0xFF
+
+    @staticmethod
+    def udp_receiver(ip, port):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server_socket.bind((ip,port))
+        data, addr = server_socket.recvfrom(1024)
+        print("received msg: %s" % data)
--- main.py
+++ main.py
@@ -10,27 +10,36 @@ class Main:
         self.host = 'broker.hivemq.com'
         self.topic = '/test'
         self.serialCom = SerialCom()
-        self.udpCom = UdpCom()
         self.msg = {bytes}
         # RS-232
         self.port = '/dev/ttyUSB0'
         self.baud = 9600
         # TCP
-        self.ip = '169.254.53.33'
-        self.tcp_port = 1111
+        self.ip = '192.168.0.22'
+        self.udp_port = 3000
 
-    def __on_message_print(self, client, username, message):
+    def __on_message_print_special_channel(self, client, username, message):
         msg = message.payload.decode('utf-8')
         msg = msg.encode('ascii', 'ignore')
         try:
-            # SerialCom.rs_sender(self.port, self.baud, msg)
-            UdpCom.tcp_sender(self.ip, self.tcp_port, msg)
+            SerialCom.rs_sender(self.port, self.baud, msg)
         except:
             print('no connection with printer')
 
+    def __on_message_print_edgraf(self, client, username, message):
+        msg = message.payload.decode('utf-8')
+        msg = msg.encode('ascii', 'ignore')
+        udpCom = UdpCom(self.ip, self.udp_port, msg)
+        #try:
+            # SerialCom.rs_sender(self.port, self.baud, msg)
+        udpCom.udp_sender()
+            #UdpCom.udp_receiver(self.ip, self.udp_port)
+        #except:
+        #    print('no connection with printer')
+
     def subscribe(self):
         try:
-            subscribe.callback(callback=self.__on_message_print, topics=self.topic, hostname=self.host)
+            subscribe.callback(callback=self.__on_message_print_edgraf, topics=self.topic, hostname=self.host)
         except socket.gaierror:
             print('no connection with broker')
         except KeyboardInterrupt:
